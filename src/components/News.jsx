e96e2c1b39bdad912e31ebd975eeb6d58602513e
Sofiadoingcode <88271469+Sofiadoingcode@users.noreply.github.com>
2022-11-10 15:21:56 +0100

    Make cards

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import facade from "../utils/apiFacade.js";
+import "../styles/card.css";
 
 function News(props) {
     const [news, setNews] = useState([])
-    useEffect(  () => {
+    useEffect(() => {
         facade.fetchNews()
             .then((res) => res = res)
             .then(data => {
                 setNews(data.articles);
             });
 
-    }, [] );
+    }, []);
 
 
     return (
@@ -18,37 +19,25 @@ function News(props) {
             <div style={{padding: "2rem"}}>
 
 
-                <div>
-                    <div>
-                        <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Pic</th>
-
-                        </tr>
-                        </thead>
-                        {
-                            news.map(
-                            new1 =>
-                                <>
-                                    <tr>
-                                        <td>{new1.title}</td>
-                                        <td>{new1.author}</td>
-                                        <td><img src={new1.urlToImage}/></td>
-
-                                    </tr>
-                                </>
-
-                        )}
-                    </div>
-
-                </div>
-
+                {
+                    news.map(
+                        new1 => <>
+                            <div className="card" style={{width: "18rem"}}>
+                                <img src={new1.urlToImage} className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{new1.title}</h5>
+                                    <p className="card-text">{new1.description}</p>
+                                    <a href={new1.url} className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </>
+                    )
+                }
             </div>
         </>
-    );
+    )
 }
+
 
 
 export default News;
